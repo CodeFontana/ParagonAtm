998985823f4bc2b5db076b7f3ee1b1851b9a4dff
Brian Fontana <brian@codefoxtrot.com>
2022-04-17 15:17:09 -0400

    Override ToString()
    For a more friendly output format

--- ConsoleUI/Models/TransactionModel.cs
+++ ConsoleUI/Models/TransactionModel.cs
@@ -1,8 +1,15 @@
-﻿namespace ConsoleUI.Models;
+﻿using System.Text.Json;
+
+namespace ConsoleUI.Models;
 
 public class TransactionModel
 {
     public string Name { get; set; }
     public TransactionOptionsModel Options { get; set; }
     public List<TransactionScreenFlowModel> ScreenFlow { get; set; }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+    }
 }
