1d23c370f2ea8123e4a6c58e3956d54ba1d8b0ad
Brian Fontana <brian@codefoxtrot.com>
2022-04-17 14:29:31 -0400

    Define PlaylistModel

---
+++ ConsoleUI/Models/PlaylistModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleUI.Models;
+
+public class PlaylistModel
+{
+    public string Name { get; set; }
+    public PlaylistOptionsModel Options { get; set; }
+    public List<string> Transactions { get; set; }
+}
---
+++ ConsoleUI/Models/PlaylistOptionsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleUI.Models;
+
+public class PlaylistOptionsModel
+{
+    public int Repeat { get; set; } = 1;
+    public int RepeatDelay { get; set; } = 10000;
+    public bool Shuffle { get; set; } = false;
+}
