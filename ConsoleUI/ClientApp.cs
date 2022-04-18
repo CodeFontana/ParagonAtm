9e6f497d1727a9d2611da17562187c596f1afc91
Brian Fontana <brian@codefoxtrot.com>
2022-04-18 10:10:58 -0400

    Remove unnecessary injects

--- ConsoleUI/ClientApp.cs
+++ ConsoleUI/ClientApp.cs
@@ -9,22 +9,16 @@ public class ClientApp : IHostedService
 {
     private readonly IHostApplicationLifetime _hostApplicationLifetime;
     private readonly ILogger<ClientApp> _logger;
-    private readonly IAgentService _agentService;
-    private readonly IConnectionService _connectionService;
     private readonly IClientService _clientService;
     private readonly IConsumerTransactionService _consumerTransactionService;
 
     public ClientApp(IHostApplicationLifetime hostApplicationLifetime,
                      ILogger<ClientApp> logger,
-                     IAgentService agentService,
-                     IConnectionService connectionService,
                      IClientService clientService,
                      IConsumerTransactionService consumerTransactionService)
     {
         _hostApplicationLifetime = hostApplicationLifetime;
         _logger = logger;
-        _agentService = agentService;
-        _connectionService = connectionService;
         _clientService = clientService;
         _consumerTransactionService = consumerTransactionService;
     }
